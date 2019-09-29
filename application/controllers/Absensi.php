9aa89a9a52b625dc2d68ab967f87bc5e15565f6c
mazeundercover <mazeundercover@gmail.com>
2019-09-30 04:22:28 +0700

    Modif Absensi.php

--- application/controllers/Absensi.php
+++ application/controllers/Absensi.php
@@ -3,10 +3,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Absensi extends CI_Controller {
 
+	public function __construct(){
+     	parent::__construct();
+        }
+
 	public function index() {
+		if (!isset($_SESSION['auth'])) {
+			redirect(base_url('/absensi/login')); 
+		}
 		$data['header']['title'] = "Absensi";
 		$this->load->view('templates/headcontent',$data);
 		$this->load->view('home/home');
 		$this->load->view('templates/footer');
+	}
+
+	public function login() {
+		echo "LOGIN";
 	}
 }
